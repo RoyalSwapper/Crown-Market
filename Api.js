ec776dc33cae46d3a1cbf4f2e2dfbc7460adf2c7
RoyalSwapper <91436039+RoyalSwapper@users.noreply.github.com>
2021-09-27 00:05:33 +0100

    Update Api.js

--- Api.js
+++ Api.js
@@ -1,10 +1,9 @@
-- 👋 Hi, I’m @RoyalSwapper
-- 👀 I’m interested in ...
-- 🌱 I’m currently learning ...
-- 💞️ I’m looking to collaborate on ...
-- 📫 How to reach me ...
+v1.0 is out !
 
-<!---
-RoyalSwapper/RoyalSwapper is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+Whats New?
+
+[+] New Skins
+[+] New BackPacks
+[+] New UI
+[+] New Music
+[+] New Discord RPC
